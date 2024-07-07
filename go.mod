ee2db4f88afb4d854eed0d072469047b9ea41a98
yuichi04 <crgone.910428.en@gmail.com>
2024-07-07 22:32:27 +0900

    ToDoアプリを作成

--- go.mod
+++ go.mod
@@ -1,10 +1,23 @@
-module github.com/yuichi04/go-todo-app
+module go-todo-app
 
 go 1.22.4
 
-require github.com/labstack/echo/v4 v4.12.0
+require (
+	github.com/labstack/echo/v4 v4.12.0
+	gorm.io/gorm v1.25.10
+)
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -14,4 +27,5 @@ require (
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gorm.io/driver/postgres v1.5.9
 )
--- main.go
+++ main.go
@@ -1,15 +1,97 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
+type Todo struct {
+	gorm.Model
+	Content string `gorm:"not null"`
+	Done    bool
+	Until   time.Time
+}
+
+type Result struct {
+	Content string `json:"content"`
+}
+
 func main() {
+	connectionString := "host=localhost user=pquser password=pqpassword dbname=pqdatabase port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+
+	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	err = db.AutoMigrate(&Todo{})
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+
 	e := echo.New()
+
+	e.POST("/todos", func(c echo.Context) error {
+		var todo Todo
+		if err := c.Bind(&todo); err != nil {
+			return err
+		}
+		if result := db.Create(&todo); result.Error != nil {
+			return result.Error
+		}
+		return c.JSON(http.StatusCreated, todo)
+	})
+
+	e.GET("/todos", func(c echo.Context) error {
+		var todos []Todo
+		if result := db.Find(&todos); result.Error != nil {
+			return result.Error
+		}
+		return c.JSON(http.StatusOK, todos)
+	})
+
+	e.GET("/todos/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		var todo Todo
+		if result := db.First(&todo, id); result.Error != nil {
+			return result.Error
+		}
+		var res Result
+		res.Content = todo.Content
+		return c.JSON(http.StatusOK, res)
+	})
+
+	e.PUT("/todos/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		var todo Todo
+		if err := c.Bind(&todo); err != nil {
+			return err
+		}
+		todo.ID = 0 // GORM が新しいレコードとして扱わないようにIDをリセット
+		if result := db.Model(&Todo{}).Where("id = ?", id).Updates(todo); result.Error != nil {
+			return result.Error
+		}
+		return c.JSON(http.StatusOK, todo)
+	})
+
+	e.DELETE("/todos/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		if result := db.Delete(&Todo{}, id); result.Error != nil {
+			return result.Error
+		}
+		return c.NoContent((http.StatusNoContent))
+	})
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "")
+		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":8989"))
 }
